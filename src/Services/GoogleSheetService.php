79c93c4a4de015457e27da2ebe61b32b351f435d
Thomas <zawlintun@robust.best>
2024-07-18 02:42:01 +0700

    update

--- src/Services/GoogleSheetService.php
+++ src/Services/GoogleSheetService.php
@@ -3,26 +3,25 @@
 namespace Zlt\LaravelGoogleSheet\Services;
 
 use Google\Service\Sheets;
+use Google\Service\Sheets\BatchUpdateSpreadsheetRequest;
+use Google\Service\Sheets\CellFormat;
+use Google\Service\Sheets\GridRange;
+use Google\Service\Sheets\Request;
 use Google\Service\Sheets\ValueRange;
 
 class GoogleSheetService
 {
-    protected Sheets $service;
+    protected ?Sheets $service = null;
 
-    public function __construct()
-    {
-        $this->init();
-    }
-
-    private function init()
+    public function __construct(array|null $config = null)
     {
         $client = new \Google\Client();
-        $config = config('filesystems.disks.google');
+        $config ??= config('filesystems.disks.google');
         $client->setScopes(Sheets::SPREADSHEETS);
         $client->setClientId($config['clientId']);
         $client->setClientSecret($config['clientSecret']);
         $client->refreshToken($config['refreshToken']);
-        $client->setAccessType('online');
+        $client->setAccessType($config['type'] ?? 'online');
         $this->service = new Sheets($client);
     }
 
@@ -98,5 +97,103 @@ class GoogleSheetService
             return array_merge(...$temp);
         }
         throw new \Exception('No Data found');
+    }
+
+
+    /**
+     * @param  string  $sheetId
+     * @param  array  $formatOptions
+     * @param  array  $rows
+     * @param  array  $columns
+     * @return bool|Sheets\BatchUpdateSpreadsheetResponse
+     * @throws \Exception
+     */
+    public function formatCells(string $sheetId, array $formatOptions, array $rows = [], array $columns = []): bool|Sheets\BatchUpdateSpreadsheetResponse
+    {
+        //[
+        //     'textFormat' => [
+        //         'fontSize' => 14,
+        //         'bold' => true,
+        //         'foregroundColor' => ['red' => 1.0] // Red color
+        //     ],
+        //     'numberFormat' => [
+        //         'type' => 'CURRENCY',
+        //         'pattern' => '$#,##0.00'
+        //     ],
+        //     'backgroundColor' => ['red' => 0.8, 'green' => 0.8, 'blue' => 0.8], // Light gray
+        //     'horizontalAlignment' => 'CENTER',
+        //     'wrapStrategy' => 'WRAP'
+        // ]);
+        $values = $this->get($sheetId);    
+
+        $format = new CellFormat($formatOptions);
+
+        $updateRequests = [];
+
+        $maxRowIndex = count($values) - 1;
+
+        foreach ($rows as $rowIndex) {
+            $updateRange = new GridRange([
+                'sheetId' => 0,
+                'startRowIndex' => $rowIndex,
+                'endRowIndex' => $rowIndex + 1, // Single row range
+                'startColumnIndex' => 0,
+                'endColumnIndex' => count($values[$rowIndex]) // All columns in the row
+            ]);
+
+            $updateRequest = new Request([
+                'updateCells' => [
+                    'range' => $updateRange,
+                    'rows' => [
+                        ['values' => array_map(function ($cell) use ($format) {
+                            return ['userEnteredFormat' => $format];
+                        }, $values[$rowIndex])]
+                    ],
+                    'fields' => 'userEnteredFormat.textFormat'
+                ]
+            ]);
+
+            $updateRequests[] = $updateRequest;
+        }
+
+        foreach ($columns as $columnIndex) {
+            $updateRange = new GridRange([
+                'sheetId' => 0,
+                'startRowIndex' => 0,
+                'endRowIndex' => $maxRowIndex + 1,
+                'startColumnIndex' => $columnIndex,
+                'endColumnIndex' => $columnIndex + 1
+            ]);
+
+            $updateRequest = [
+                'updateCells' => [
+                    'range' => $updateRange,
+                    'rows' => [],
+                    'fields' => 'userEnteredFormat.textFormat'
+                ]
+            ];
+
+            // Apply bold format to each row in the specified column
+            foreach ($values as $rowIndex => $row) {
+                if (isset($row[$columnIndex])) {
+                    $updateRequest['updateCells']['rows'][$rowIndex] = [
+                        'values' => [
+                            ['userEnteredFormat' => $format]
+                        ]
+                    ];
+                }
+            }
+
+            $updateRequests[] = new Request($updateRequest);
+        }
+
+        if(empty($updateRequests)){
+            return false;
+        }
+
+        $batchUpdateRequest = new BatchUpdateSpreadsheetRequest([
+            'requests' => $updateRequests
+        ]);
+        return $this->service->spreadsheets->batchUpdate($sheetId, $batchUpdateRequest);
     }
 }
